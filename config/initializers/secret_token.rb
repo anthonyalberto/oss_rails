8900c54c209d0b206d3e4abfec088d115fc2b651
Anthony Alberto <alberto.anthony@gmail.com>
2013-02-08 11:29:42 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OssRails::Application.config.secret_token = 'de3370edb4a848d31428ccc83a429b2141073fdcecc2a617c36ffa7514d69e9d63ee0ca8341094fd07eaf9a2d2f556344f8c64f5cc087362a34444866281d29b'
